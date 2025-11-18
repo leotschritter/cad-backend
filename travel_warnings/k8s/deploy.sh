fb6547048e0a1112e5014d3f64a1485854748f21
Leo Tschritter <leotschritter@gmail.com>
2025-11-18 11:47:40 +0100

    feat: pipeline deploys to gcloud cluster

--- travel_warnings/k8s/deploy.sh
+++ travel_warnings/k8s/deploy.sh
@@ -5,7 +5,7 @@ kubectl apply -f app-config.yaml
 kubectl apply -f postgres-pvc.yaml
 kubectl apply -f postgres.yaml
 kubectl apply -f travel-warnings-api.yaml
-kubectl apply -f ingress.yaml
+kubectl apply -f ingress-dev.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 kubectl rollout status deployment/travel-warnings-api --timeout=240s
 # check status of all pods
