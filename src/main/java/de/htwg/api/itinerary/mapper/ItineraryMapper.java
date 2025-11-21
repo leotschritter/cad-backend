d7a391174a311b83c1aaa54a881ebf416bb8b770
dennishoang <dennishoang199@gmail.com>
2025-11-21 13:53:17 +0100

    added endpoint for post image urls in db and endpoint for retrieving image urls for user

--- src/main/java/de/htwg/api/itinerary/mapper/ItineraryMapper.java
+++ src/main/java/de/htwg/api/itinerary/mapper/ItineraryMapper.java
@@ -43,6 +43,7 @@ public class ItineraryMapper {
                 .shortDescription(entity.getShortDescription())
                 .detailedDescription(entity.getDetailedDescription())
                 .userName(entity.getUser() != null ? entity.getUser().getName() : null)
+                .userEmail(entity.getUser() != null ? entity.getUser().getEmail() : null)
                 .build();
     }
 
