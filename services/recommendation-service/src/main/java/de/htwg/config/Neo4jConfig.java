d5faa16ed7049dde71832a6675f324449c887f6f
Leo Tschritter <leotschritter@gmail.com>
2026-01-14 12:22:02 +0100

    refactor: update Neo4j configuration properties to remove Quarkus prefix

--- services/recommendation-service/src/main/java/de/htwg/config/Neo4jConfig.java
+++ services/recommendation-service/src/main/java/de/htwg/config/Neo4jConfig.java
@@ -17,13 +17,13 @@ public class Neo4jConfig {
 
     private static final Logger LOG = Logger.getLogger(Neo4jConfig.class);
 
-    @ConfigProperty(name = "quarkus.neo4j.uri")
+    @ConfigProperty(name = "neo4j.uri")
     String uri;
 
-    @ConfigProperty(name = "quarkus.neo4j.authentication.username")
+    @ConfigProperty(name = "neo4j.authentication.username")
     String username;
 
-    @ConfigProperty(name = "quarkus.neo4j.authentication.password")
+    @ConfigProperty(name = "neo4j.authentication.password")
     String password;
 
     @Produces
